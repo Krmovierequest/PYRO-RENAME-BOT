564352a9980a41d52a529230408f83c1aa48d33c
Kdbhaifreefire <116331949+Krmovierequest@users.noreply.github.com>
2024-08-06 06:46:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,13 +26,13 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "12850056")
+    API_HASH  = os.environ.get("API_HASH", "15564ec4a1a2cbef87c99a9aa9e40b34")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6656522620:AAGLN6e3rYujnB_T7IhWJt7emZj5biv63mE") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","kdbhaifreefire")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://kdbhaifreefire:7872278427kd@kdbhaifreefire.sqwqyrn.mongodb.net/?retryWrites=true&w=majority&appName=Kdbhaifreefire")
  
     # other configs
     BOT_UPTIME  = time.time()
